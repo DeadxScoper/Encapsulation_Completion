354107d13c87f9ec9873c1e79b78ac88d71bb37d
HSA-Student <student.e.amofa@horiozncolumbus.org>
2020-10-11 12:50:02 -0400

    initial project

--- src/org/horizoncolumbus/hs/Main.java
+++ src/org/horizoncolumbus/hs/Main.java
@@ -4,5 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Employee farah = new Employee(1000000);
+        // System.out.println(farah.salary);
+
+        //initial salary
+        System.out.println("$" + farah.getSalary());
+
+        // 10 percent increase
+        farah.setSalary((int) ((farah.getSalary() * .1) + farah.getSalary()));
+        System.out.println("$" + farah.getSalary());
+
     }
 }
+
+
